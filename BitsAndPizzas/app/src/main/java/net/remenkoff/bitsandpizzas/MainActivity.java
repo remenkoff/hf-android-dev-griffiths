e1ae7c8cf81b3d15287c80777487475fb690f1e1
Ruslan Remenkov <remenkoff@gmail.com>
2019-11-28 16:01:06 +0300

    Add BitsAndPizzas app and configure theme appearance

---
+++ BitsAndPizzas/app/src/main/java/net/remenkoff/bitsandpizzas/MainActivity.java
@@ -0,0 +1,15 @@
+package net.remenkoff.bitsandpizzas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // MARK: - Activity Lifecycle
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
