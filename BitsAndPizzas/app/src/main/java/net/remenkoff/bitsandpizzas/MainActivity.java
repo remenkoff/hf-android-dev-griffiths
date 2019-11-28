e73b6bfbd832a0fa19a6f840c7a151cd99372da9
Ruslan Remenkov <remenkoff@gmail.com>
2019-11-28 16:11:12 +0300

    Replace ActionBar with Toolbar (androidx)

--- BitsAndPizzas/app/src/main/java/net/remenkoff/bitsandpizzas/MainActivity.java
+++ BitsAndPizzas/app/src/main/java/net/remenkoff/bitsandpizzas/MainActivity.java
@@ -1,6 +1,7 @@
 package net.remenkoff.bitsandpizzas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,7 +10,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupInitialState();
+    }
+
+    // MARK: - Private Instance Interface
+    private void setupInitialState() {
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 
 }
