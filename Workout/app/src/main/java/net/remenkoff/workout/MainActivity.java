e12ef41b2e4560e46b584c647cfb0f9fa2506473
Ruslan Remenkov <remenkoff@gmail.com>
2019-11-29 18:45:28 +0300

    Add supporting of large layouts (tablet devices) in Workout app

--- Workout/app/src/main/java/net/remenkoff/workout/MainActivity.java
+++ Workout/app/src/main/java/net/remenkoff/workout/MainActivity.java
@@ -1,10 +1,11 @@
 package net.remenkoff.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
 
@@ -18,9 +19,27 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
     // MARK: - WorkoutListFragment.Listener
     @Override
     public void didSelectItem(long index) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.K_WORKOUT_ID_KEY, (int) index);
-        startActivity(intent);
+        int selectedWorkoutId = (int)index;
+
+        FrameLayout fragmentContainerLayout = findViewById(R.id.detail_frag_container);
+        boolean isFragmentContainerExists = fragmentContainerLayout != null;
+
+        if (isFragmentContainerExists) {
+            WorkoutDetailFragment workoutDetailFragment = new WorkoutDetailFragment();
+            workoutDetailFragment.setWorkoutId(selectedWorkoutId);
+
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.detail_frag_container, workoutDetailFragment);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+
+        } else {
+
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.K_WORKOUT_ID_KEY, selectedWorkoutId);
+            startActivity(intent);
+        }
     }
 
 }
