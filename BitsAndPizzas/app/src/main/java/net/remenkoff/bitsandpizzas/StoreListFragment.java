d1b51bf846d92c96eb1286bfeae6de530d22d3e1
Ruslan Remenkov <remenkoff@gmail.com>
2019-11-30 13:03:38 +0300

    Add past, pizza, stores and top fragment to the BitsAndPizzas app

---
+++ BitsAndPizzas/app/src/main/java/net/remenkoff/bitsandpizzas/StoreListFragment.java
@@ -0,0 +1,32 @@
+package net.remenkoff.bitsandpizzas;
+
+import androidx.fragment.app.ListFragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class StoreListFragment extends ListFragment {
+
+    // MARK: - Fragment Lifecycle
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setupAdapter(inflater.getContext());
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    // MARK: - Private Instance Interface
+    private void setupAdapter(Context context) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores)
+        );
+        setListAdapter(adapter);
+    }
+
+}
---
+++ BitsAndPizzas/app/src/main/java/net/remenkoff/bitsandpizzas/TopFragment.java
@@ -0,0 +1,20 @@
+package net.remenkoff.bitsandpizzas;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopFragment extends Fragment {
+
+    // MARK: - Fragment Lifecycle
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+
+}
