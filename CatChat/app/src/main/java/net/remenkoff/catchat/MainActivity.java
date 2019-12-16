4b5913ea47cf261f96e0502ff80b4327b0165a4b
Ruslan Remenkov <remenkoff@gmail.com>
2019-12-16 15:57:19 +0300

    Add pullable side menu (DrawerLayout)

--- CatChat/app/src/main/java/net/remenkoff/catchat/MainActivity.java
+++ CatChat/app/src/main/java/net/remenkoff/catchat/MainActivity.java
@@ -1,14 +1,40 @@
 package net.remenkoff.catchat;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // MARK: - Activity Lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.nav_open_drawer,
+            R.string.nav_close_drawer
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.content_frame, fragment);
+        fragmentTransaction.commit();
     }
+
 }
