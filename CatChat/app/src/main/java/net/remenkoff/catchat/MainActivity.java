17bc2657429372d8c6edb37457fd55abe77ffa8c
Ruslan Remenkov <remenkoff@gmail.com>
2019-12-16 16:35:25 +0300

    Add handlers for all menu items

--- CatChat/app/src/main/java/net/remenkoff/catchat/MainActivity.java
+++ CatChat/app/src/main/java/net/remenkoff/catchat/MainActivity.java
@@ -1,15 +1,23 @@
 package net.remenkoff.catchat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // MARK: - Activity Lifecycle
     @Override
@@ -31,10 +39,68 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         Fragment fragment = new InboxFragment();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.content_frame, fragment);
         fragmentTransaction.commit();
+    }
+
+    // MARK: - NavigationView.OnNavigationItemSelectedListener
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_inbox:
+                fragment = new InboxFragment();
+                break;
+
+            case R.id.nav_drafts:
+                fragment = new DraftsFragment();
+                break;
+
+            case R.id.nav_sent:
+                fragment = new SentItemsFragment();
+                break;
+
+            case R.id.nav_trash:
+                fragment = new TrashFragment();
+                break;
+
+            case R.id.nav_help:
+                intent = new Intent(this, HelpActivity.class);
+                break;
+
+            case R.id.nav_feedback:
+                intent = new Intent(this, FeedbackActivity.class);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Such a menu item is not supported!");
+        }
+
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment);
+            fragmentTransaction.commit();
+
+            DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+            return true;
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+            return true;
+        }
+
+        return false;
     }
 
 }
