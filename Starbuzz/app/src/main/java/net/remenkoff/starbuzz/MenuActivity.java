f4c16847d8d159b76f8d68be086116e0c2d75d87
Ruslan Remenkov <remenkoff@gmail.com>
2020-05-03 18:32:06 +0300

    Add favorite list to the main menu, update favorites data onRestart()

--- Starbuzz/app/src/main/java/net/remenkoff/starbuzz/MenuActivity.java
+++ Starbuzz/app/src/main/java/net/remenkoff/starbuzz/MenuActivity.java
@@ -2,21 +2,58 @@ package net.remenkoff.starbuzz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MenuActivity extends Activity {
 
     // MARK: - Private Instance Properties
     private MenuLayout layout;
 
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
+
     // MARK: - Activity Lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupInitialState();
+        setupOptionsListView();
+        setupFavoritesListVew();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Cursor newCursor = db.query(
+                "DRINK",
+                new String[] {"_id", "NAME"},
+                "FAVORITE = 1",
+                null,
+                null,
+                null,
+                null
+        );
+
+        CursorAdapter adapter = (CursorAdapter) layout.favoritesListView.getAdapter();
+        adapter.changeCursor(newCursor);
+        favoritesCursor = newCursor;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
     }
 
     // MARK: - Private Instance Interface
@@ -45,4 +82,55 @@ public class MenuActivity extends Activity {
         setContentView(layout);
     }
 
+    private void setupOptionsListView() {
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MenuActivity.this, DrinkDetailsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        layout.favoritesListView.setOnItemClickListener(itemClickListener);
+    }
+
+    private void setupFavoritesListVew() {
+        try {
+            SQLiteOpenHelper dbHelper = new StarbuzzDatabaseHelper(this);
+            db = dbHelper.getReadableDatabase();
+
+            favoritesCursor = db.query(
+                    "DRINK",
+                    new String[] {"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null,
+                    null,
+                    null,
+                    null
+            );
+
+            CursorAdapter favoritesAdapter = new SimpleCursorAdapter(
+                    MenuActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[] {"NAME"},
+                    new int[] {android.R.id.text1},
+                    0
+            );
+            layout.favoritesListView.setAdapter(favoritesAdapter);
+
+        } catch(SQLiteException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+
+        layout.favoritesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MenuActivity.this, DrinkDetailsActivity.class);
+                intent.putExtra(DrinkDetailsActivity.K_DRINK_ID_KEY, (int)id);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- Starbuzz/app/src/main/java/net/remenkoff/starbuzz/MenuLayout.java
+++ Starbuzz/app/src/main/java/net/remenkoff/starbuzz/MenuLayout.java
@@ -1,10 +1,14 @@
 package net.remenkoff.starbuzz;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 
 final class MenuLayout extends LinearLayout {
 
@@ -12,9 +16,10 @@ final class MenuLayout extends LinearLayout {
     private final int K_DEFAULT_PADDING = 16;
 
     // MARK: - Public Instance Properties
-    public final ImageView logoImageView;
-    public final ListView menuListView;
-
+    final ImageView logoImageView;
+    final ListView menuListView;
+    final TextView favoritesTextView;
+    final ListView favoritesListView;
 
     // MARK: - Instantiation
     public MenuLayout(Context context) {
@@ -22,36 +27,46 @@ final class MenuLayout extends LinearLayout {
 
         logoImageView = new ImageView(context);
         menuListView = new ListView(context);
+        favoritesTextView = new TextView(context);
+        favoritesListView = new ListView(context);
 
-        setupInitialState();
+        configureLayout();
     }
 
     // MARK: - Private Instance Interface
-    private void setupInitialState() {
-        setupLayout();
-        setupLogoImageView();
-        setupMenuListView();
-    }
-
-    private void setupLayout() {
+    private void configureLayout() {
         LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
         setLayoutParams(params);
         setOrientation(VERTICAL);
         setGravity(Gravity.CENTER);
         setPadding(K_DEFAULT_PADDING, K_DEFAULT_PADDING, K_DEFAULT_PADDING, K_DEFAULT_PADDING);
 
-        addView(logoImageView);
-        addView(menuListView);
+        addView(configuredLogoImageView());
+        addView(configuredMenuListView());
+        addView(configuredFavoritesTextView());
+        addView(configuredFavoritesListView());
     }
 
-    private void setupLogoImageView() {
+    private ImageView configuredLogoImageView() {
         LayoutParams params = new LayoutParams(200, 100);
         logoImageView.setLayoutParams(params);
+        return logoImageView;
     }
 
-    private void setupMenuListView() {
+    private ListView configuredMenuListView() {
         LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
         menuListView.setLayoutParams(params);
+        return menuListView;
     }
 
+    private TextView configuredFavoritesTextView() {
+        favoritesTextView.setText(getResources().getText(R.string.favorites));
+        favoritesTextView.setTextAppearance(android.R.style.TextAppearance_Large);
+        return favoritesTextView;
+    }
+
+    private ListView configuredFavoritesListView() {
+        favoritesListView.setId(getResources().getIdentifier("list_favs", null, null));
+        return favoritesListView;
+    }
 }
