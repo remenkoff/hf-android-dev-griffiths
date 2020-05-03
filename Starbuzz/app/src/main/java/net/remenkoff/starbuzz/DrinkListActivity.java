c63b938f7a4dce47fd8286eb8734abd6d2e6a513
Ruslan Remenkov <remenkoff@gmail.com>
2020-05-03 14:38:34 +0300

    Change data provider to Starbuzz SQLite database for drink list

--- Starbuzz/app/src/main/java/net/remenkoff/starbuzz/DrinkListActivity.java
+++ Starbuzz/app/src/main/java/net/remenkoff/starbuzz/DrinkListActivity.java
@@ -2,15 +2,23 @@ package net.remenkoff.starbuzz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public final class DrinkListActivity extends Activity {
 
     // MARK: - Private Instance Properties
     private DrinkListLayout layout;
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     // MARK: - Activity Lifecycle
     @Override
@@ -19,16 +27,45 @@ public final class DrinkListActivity extends Activity {
         setupInitialState();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
     // MARK: - Private Instance Interface
     private void setupInitialState() {
         layout = new DrinkListLayout(this);
 
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks
-        );
-        layout.drinkListView.setAdapter(listAdapter);
+        SQLiteOpenHelper dbHelper = new StarbuzzDatabaseHelper(this);
+
+        try {
+            db = dbHelper.getReadableDatabase();
+            cursor = db.query(
+                    "DRINK",
+                    new String[] {"_id", "NAME"},
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[] {"NAME"},
+                    new int[] {android.R.id.text1},
+                    0
+            );
+
+            layout.drinkListView.setAdapter(listAdapter);
+
+        } catch(SQLiteException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
 
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
